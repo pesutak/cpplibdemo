30483fe23ec5b457c10a35be5744ba184f5c41e9
pesutak <peter@sutak.net>
2018-12-03 15:43:16 +0100

    added class instance call examples

--- libdynamic/libdynamic.h
+++ libdynamic/libdynamic.h
@@ -6,4 +6,33 @@
 #define LIBDYNAMIC_API __declspec(dllimport)
 #endif
 
-extern "C" LIBDYNAMIC_API int dynFn(const int a);
+#ifdef __cplusplus
+#define EXTERN_C     extern "C"
+#else
+#define EXTERN_C
+#endif // __cplusplus
+
+#ifdef __cplusplus
+struct IWorker {
+public:
+	virtual int DoWork(const int a) = 0;
+	virtual void Release() = 0;
+};
+
+class LIBDYNAMIC_API Worker {
+public:
+	int DoWork(const int a);
+};
+
+
+typedef IWorker* WORKERHANDLE;
+#else
+typedef struct tagWORKERHANDLE {} *WORKERHANDLE;
+#endif
+
+// c-style declarations
+EXTERN_C LIBDYNAMIC_API WORKERHANDLE WINAPI GetWorker(VOID);
+EXTERN_C LIBDYNAMIC_API INT APIENTRY WorkerDoWork(WORKERHANDLE handle, INT n);
+EXTERN_C LIBDYNAMIC_API VOID APIENTRY WorkerRelease(WORKERHANDLE handle);
+
+EXTERN_C LIBDYNAMIC_API int dynFn(const int a);
